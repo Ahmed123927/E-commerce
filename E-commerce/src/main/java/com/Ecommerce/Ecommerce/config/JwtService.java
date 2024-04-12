bd901690a1600f9bbc54ccaa77179bd401555eeb
Ahmed123927 <ah7070652@gmail.com>
2024-04-12 23:53:05 +0200

    finshing security

--- E-commerce/src/main/java/com/Ecommerce/Ecommerce/config/JwtService.java
+++ E-commerce/src/main/java/com/Ecommerce/Ecommerce/config/JwtService.java
@@ -2,16 +2,35 @@ package com.Ecommerce.Ecommerce.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
 public class JwtService {
     private static final String SECRET_KEY="31f6de8a20e1e206a77e6f6ec319353520e8f2c8f8f428841c8707ef3b32776e";
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails){
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*24))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(),userDetails);
+    }
     public String extractUsername(String token) {
         return extractClaim(token,Claims::getSubject);
     }
@@ -25,6 +44,19 @@ public class JwtService {
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
+    }
+
+    public boolean isTokenValid(String token,UserDetails userDetails){
+        final String username=extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token,Claims::getExpiration);
     }
 
     private Key getSignKey() {
